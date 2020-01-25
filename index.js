a18484cdd17d1e87d23154ec82edca3a88bf95e7
fabio <fabio>
2020-01-25 11:18:23 -0300

    Alterando retorno da Rota

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express =  require('express');
 
 const app = express();
+const tese = 'Nome';
 
 app.get('/teste', (request, response) => {
-    return response.json({hello: 'word'})
+    return response.json({hello: 'Hello Word'})
 });
 
 app.listen(3333);
